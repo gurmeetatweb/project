07e204945ee68eb2c4b5a8e5a3f45cee49477a69
Gurmeet Singh <gurmeetatweb@gmail.com>
2025-05-18 03:09:08 +0530

    Remove wordcloud module

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from wordcloud import WordCloud
+#from wordcloud import WordCloud
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
